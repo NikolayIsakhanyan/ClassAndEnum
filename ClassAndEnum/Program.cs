bbfbab57c1d9c8b3a3526459d3543f2b5c933dd7
Isakhanyan Nikolay <isaxanyan23@outlook.com>
2022-12-06 18:40:04 +0400

    Add project files.

---
+++ ClassAndEnum/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more informati
+using ClassAndEnum;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Dog=0");
+        Console.WriteLine("Cat=1");
+        Console.WriteLine("Fish=2");
+        Console.WriteLine("Birds=3");
+        int a = Convert.ToInt16(Console.ReadLine());
+        switch (a)
+        {
+            case 0:
+                string dog = AnimalClass.Haff.ToString();
+                Console.WriteLine(dog);
+                 break;
+            case 1:
+                string cat = AnimalClass.Myau.ToString();
+                Console.WriteLine(cat);
+                break;
+            case 2:
+                string fish = AnimalClass.Blk.ToString();
+                Console.WriteLine(fish);
+                break;
+            case 3:
+                string birds = AnimalClass.Tiv.ToString();
+                Console.WriteLine(birds);
+                break;
+            default:
+                break;
+
+        }
+
+
+
+
+
+
+    }
+}
